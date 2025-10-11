d2367b1849e71c2c254f59c09985425f1fc36719
tama235 <al25022@shibaura-it.ac.jp>
2025-10-11 18:07:57 +0900

    login

--- App.tsx
+++ App.tsx
@@ -1,9 +1,8 @@
-
-
-import React from 'react';
+import React from "react";
 // プロジェクトが元々使っていたナビゲーターをインポート
 // このパスが正しいナビゲーション定義ファイルであると仮定します
-import { AppNavigator } from './src/navigation/AppNavigator'; 
+import { AppNavigator } from "./src/navigation/AppNavigator";
+import "react-native-url-polyfill/auto";
 
 export default function App() {
   // AppNavigatorが全ての画面とルーティングを管理します
